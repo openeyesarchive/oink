87a7c47ed96ded0a6370a9c7e7d6f5fa21c5b6c2
Oli Wilkie <oliverwilkie@gmail.com>
2014-01-20 12:56:24 +0000

    Emptied oink-common

--- oink-common/src/main/java/uk/org/openeyes/oink/finders/PatientFinder.java
+++
@@ -1,9 +0,0 @@
-package uk.org.openeyes.oink.finders;
-
-import java.util.List;
-
-import uk.org.openeyes.oink.domain.Patient;
-
-public interface PatientFinder {
-	List<Patient> getPatientsById(String identifierLikePattern);
-}
