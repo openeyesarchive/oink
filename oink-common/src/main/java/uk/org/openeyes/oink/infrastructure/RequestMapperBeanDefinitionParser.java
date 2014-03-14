dfded65bd5f41bf721c1042a7078fd50d28c1f59
oli <oliverwilkie@gmail.com>
2014-03-14 10:26:43 +0000

    Improved RequestMapperBeanDefinitionParser, if no method is defined in a mapping we assume ANY§

--- oink-common/src/main/java/uk/org/openeyes/oink/infrastructure/RequestMapperBeanDefinitionParser.java
+++ oink-common/src/main/java/uk/org/openeyes/oink/infrastructure/RequestMapperBeanDefinitionParser.java
@@ -7,7 +7,7 @@ import uk.org.openeyes.oink.domain.HttpMethod;
 public class RequestMapperBeanDefinitionParser extends AbstractSingleBeanDefinitionParser  {
 
 	public HttpMethod parseMethod(String method) {
-		if(method == null) {
+		if(method == null || method.isEmpty()) {
 			return HttpMethod.ANY;
 		} else {
 			return HttpMethod.valueOf(method.toUpperCase());
