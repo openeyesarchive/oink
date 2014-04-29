89dd16d239068c71115d15f5652d514918cb0f93
oli <oliverwilkie@gmail.com>
2014-04-29 12:13:38 +0100

    Added script for simple packaging of OINK Kar and Config

---
+++ scripts/package.sh
@@ -0,0 +1,30 @@
+#!/bin/bash 
+
+# This unix script checks out OINK, builds it, and produces a tar.gz containing three files for deployment; the application, the config and a README
+# Requires GIT and Maven 3
+
+rm -rf build
+mkdir build
+
+# Checkout source from git
+git clone -b develop git@github.com:openeyes/oink.git build/source
+
+# Build and install
+mvn -f build/source/pom.xml clean install -DskipTests=true
+
+# Get project version
+VERSION=`mvn -f build/source/pom.xml help:evaluate -Dexpression=project.version 2>/dev/null| grep  "^[0-9]" | perl -ne 'chomp and print'`
+
+mkdir build/archive
+
+# Build a readme with instructions
+echo "README HERE" > build/archive/README
+
+# Build a dummy cfg for facade example
+cp build/source/oink-examples/oink-example-facade/src/main/resources/facade.properties build/archive/uk.org.openeyes.oink.facade.cfg
+
+# Copy KAR
+cp build/source/platforms/karaf/kar/target/kar-$VERSION.kar build/archive/kar-$VERSION.kar
+
+# Build tar
+cd build/archive && tar cvzf ../../archive.tar.gz * && cd ../..
