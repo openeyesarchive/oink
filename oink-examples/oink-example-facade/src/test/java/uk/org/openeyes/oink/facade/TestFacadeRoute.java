88dcd7095607b115ee3abd95ab40ca955ca15774
oli <oliverwilkie@gmail.com>
2014-04-30 10:31:36 +0100

    Route can now be run in both OSGi and non-OSGi environments

---
+++ oink-examples/oink-example-facade/src/test/java/uk/org/openeyes/oink/facade/TestFacadeRoute.java
@@ -0,0 +1,11 @@
+package uk.org.openeyes.oink.facade;
+
+import org.junit.runner.RunWith;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(locations={"classpath:META-INF/spring/camel-context.xml","classpath:"})
+public class TestFacadeRoute {
+
+}
