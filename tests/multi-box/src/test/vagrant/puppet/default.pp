a95325bb0b630764a21bd89a6c9e1d304637b23d
oli <oliverwilkie@gmail.com>
2014-06-05 15:09:31 +0100

    Simplified Java JDK provisioning and setting of JAVA_HOME

--- tests/multi-box/src/test/vagrant/puppet/default.pp
+++ tests/multi-box/src/test/vagrant/puppet/default.pp
@@ -4,11 +4,15 @@ node default {
 		command => '/usr/bin/apt-get update',
 	}
 
-	include '::rabbitmq'
-	include '::java'
-            
-    exec { 'setup' :
-        require => Service['rabbitmq-server'],
-        command =>  "/vagrant/guests/${endpoint}/setup.sh",
-        path => '/usr/bin:/sbin:/bin'}
+	# JAVA
+	package { 'openjdk-7-jdk':
+		ensure => present,
+	}
+
+	file { "/etc/profile.d/set_java_home.sh":
+		ensure => present,
+		content => "JAVA_HOME=$(readlink -f /usr/bin/java | sed \"s:bin/java::\")",
+		require => Package['openjdk-7-jdk']
+	}
+
 }
