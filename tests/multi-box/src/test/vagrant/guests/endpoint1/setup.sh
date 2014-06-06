ef834490b420df5ac300f491df6b9919366a87b4
Mark Sinclair <mark.sinclair@fieldofgreen.co.uk>
2014-06-06 16:35:52 +0100

    Shovel plugin setup

--- tests/multi-box/src/test/vagrant/guests/endpoint1/setup.sh
+++ tests/multi-box/src/test/vagrant/guests/endpoint1/setup.sh
@@ -5,6 +5,14 @@ sudo rabbitmqctl add_user oinkendpoint1 Test1571
 sudo rabbitmqctl set_permissions -p / oinkendpoint1 ".*" ".*" ".*"
 sudo rabbitmqctl set_user_tags oinkendpoint1 management
 
+# Setup RabbitMQ dynamic shovels
+sudo rabbitmq-plugins enable rabbitmq_management
+sudo rabbitmq-plugins enable rabbitmq_shovel
+sudo rabbitmq-plugins enable rabbitmq_shovel_management
+sudo service rabbitmq-server restart
+
+sudo rabbitmqctl set_parameter shovel "oink_out_shovel" '{"src-uri": "amqp://oinkendpoint1:Test1571@10.0.115.2:5672/", "src-queue": "pas.hl7v2.in", "dest-uri": "amqp://oinkendpoint2:Test1571@10.0.115.3:5672/", "dest-exchange": "test"}'
+
 # Move oink to correct location
 sudo mkdir -p /opt/oink
 sudo chown -R `whoami` /opt/oink
@@ -25,12 +33,13 @@ export JAVA_HOME=$JAVA_HOME
 echo "Attempting to connect to karaf"
 ./bin/client -r 20 -d 6 "echo"
 
-#Enable facade
+# Enable facade
 ./bin/client "oink:enable oink-adapter-facade /vagrant/guests/endpoint1/facade.properties"
 
-#Enable proxy
+# Enable proxy
 ./bin/client "oink:enable oink-adapter-proxy /vagrant/guests/endpoint1/proxy.properties"
 
 popd
 
 popd
+
