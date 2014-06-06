a6f85b94b9335583d82a0e9d376a5cc7d8afc994
oli <oliverwilkie@gmail.com>
2014-06-06 14:11:16 +0100

    Added setup script for endpoint2

--- tests/multi-box/src/test/vagrant/guests/endpoint2/setup.sh
+++ tests/multi-box/src/test/vagrant/guests/endpoint2/setup.sh
@@ -1,5 +1,33 @@
 #!/bin/bash
 
-sudo rabbitmqctl add_user oinkendpoint2 Test1571
+# Configure RabbitMQ User
+sudo rabbitmqctl add_user oinkendpoint1 Test1571
 sudo rabbitmqctl set_permissions -p / oinkendpoint2 ".*" ".*" ".*"
 sudo rabbitmqctl set_user_tags oinkendpoint2 management
+
+# Move oink to correct location
+sudo mkdir -p /opt/oink
+sudo chown -R `whoami` /opt/oink
+
+mkdir -p /opt/oink/settings
+pushd .
+cd /opt/oink
+cp /vagrant/vfs/distro-*.tar.gz .
+tar -zxvf distro-*.tar.gz
+
+# Start Karaf
+pushd .
+cd distro-*
+export JAVA_HOME=$JAVA_HOME
+./bin/start
+
+#Wait for it to start
+echo "Attempting to connect to karaf"
+./bin/client -r 20 -d 6 "echo"
+
+#Enable hl7v2
+./bin/client "oink:enable oink-adapter-hl7v2 /vagrant/guests/endpoint2/hl7v2.properties"
+
+popd
+
+popd
