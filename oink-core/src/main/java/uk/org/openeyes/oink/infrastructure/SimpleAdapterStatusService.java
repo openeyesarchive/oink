f28081cff2c8d9b60e3acff19c612ffcf7e9d14b
oli <oliverwilkie@gmail.com>
2014-06-06 13:18:17 +0100

    Oink Status command is now CamelContextAware so will report a failure if Spring Starts but Camel Routes dont

--- oink-core/src/main/java/uk/org/openeyes/oink/infrastructure/SimpleAdapterStatusService.java
+++ oink-core/src/main/java/uk/org/openeyes/oink/infrastructure/SimpleAdapterStatusService.java
@@ -1,10 +1,29 @@
 package uk.org.openeyes.oink.infrastructure;
 
-public class SimpleAdapterStatusService implements AdapterStatusService {
+import org.apache.camel.CamelContext;
+import org.apache.camel.CamelContextAware;
 
+public class SimpleAdapterStatusService implements AdapterStatusService, CamelContextAware {
+
+	private CamelContext camelContext;
+	
 	@Override
 	public boolean isAlive() {
-		return true;
+		if (camelContext == null) {
+			return false;
+		} else {
+			return camelContext.getStatus().isStarted();
+		}
+	}
+
+	@Override
+	public CamelContext getCamelContext() {
+		return camelContext;
+	}
+
+	@Override
+	public void setCamelContext(CamelContext camelContext) {
+		this.camelContext = camelContext;
 	}
 
 }
