1b9db99ae7cdacdd6000630c9e3b69c95eeeddb8
oli <oliverwilkie@gmail.com>
2014-06-17 07:45:09 +0100

    Added dead letter queue functionality, needs a test to be written

---
+++ oink-core/src/main/java/uk/org/openeyes/oink/hl7v2/UnsupportedHl7v2MessageException.java
@@ -0,0 +1,9 @@
+package uk.org.openeyes.oink.hl7v2;
+
+import uk.org.openeyes.oink.exception.OinkException;
+
+public class UnsupportedHl7v2MessageException extends OinkException {
+
+	private static final long serialVersionUID = 1L;
+
+}
