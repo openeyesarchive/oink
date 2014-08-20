680b9610523303d662c9b34df78ac1c1796abc65
Mark Sinclair <mark.sinclair@fieldofgreen.co.uk>
2014-08-20 11:50:49 +0100

    Hard coded /opt as install location - unlikely to be anything, but can be changed if needed, avoids confusion if fixed. Added copyright notices.

--- production/src/main/resources/install.sh
+++ production/src/main/resources/install.sh
@@ -1,13 +1,29 @@
 #!/bin/bash
+#*******************************************************************************
+# OINK - Copyright (c) 2014 OpenEyes Foundation
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#*******************************************************************************
 
 # Usage:
-#         install <TARGET_BASE_DIRECTORY> <SETTINGS_FILE>
+#         install <SETTINGS_FILE>
 
 set -e 
 
 OINK_VERSION=0.4-SNAPSHOT
-TARGET_BASE_DIRECTORY=$1
-SETTINGS_FILE=$2
+TARGET_BASE_DIRECTORY=/opt
+SETTINGS_FILE=$1
 
 # Copy OINK distro to destination
 sudo cp oink-$OINK_VERSION.tar.gz $TARGET_BASE_DIRECTORY
